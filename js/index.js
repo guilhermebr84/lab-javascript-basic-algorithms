525d62510dc58c4b26144c348827d2dfec1dd89f
Guilherme Rosa <guilhermebr84@gmail.com>
2022-01-15 10:50:49 -0300

    atualização

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 // Iteration 1: Names and Input
+let hacker1 = 'carlos'
+
+console.log (`the driver´s name is ${hacker1}`);
+
+let hacker2 = 'alberto'
+
+console.log (`the navigator´s name is ${alberto}`);
+
 
 // Iteration 2: Conditionals
 
